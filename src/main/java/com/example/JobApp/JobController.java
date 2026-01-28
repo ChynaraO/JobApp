ea6d2f453b330ebbe21c4205aa21203d82a2c1cd
ChynaraO <omurzakova_chyna@mail.ru>
2026-01-28 16:03:51 -0600

    Working with layers

--- src/main/java/com/example/JobApp/JobController.java
+++ src/main/java/com/example/JobApp/JobController.java
@@ -1,6 +1,8 @@
 package com.example.JobApp;
 
 import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JobController {
 
-    @GetMapping ({"/", "home"})
+    @Autowired
+    JobService service;
 
+    @GetMapping ({"/", "home"})
     public String home(){
         return "home";
     }
@@ -21,6 +25,7 @@ public class JobController {
     }
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost){
+        service.addJob(jobPost);
         return "success";
     }
 
